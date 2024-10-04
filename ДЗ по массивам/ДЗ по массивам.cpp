ec4276d2b2b3e0f4708216ab5d00ccb174c94c74
ADMIN <ADMIN@>
2024-10-04 10:17:16 +0300

    Добавьте файлы проекта.

---
+++ ДЗ по массивам/ДЗ по массивам.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    //Задание 1
+    int list1[] = { 9, 0, -3, 22, -19, 4, 5 };
+    float Sum = { 0.0 };
+    float sred = { 0.0 };
+    for (int i : list1)
+    {
+        Sum += i;
+    }
+    cout << "Сумма значений элементов массива с циклом for: " << Sum << endl;
+ 
+    /*
+    int i = 0;  // Сбрасываем индекс для суммирования
+    while (i < size(list1))
+    {
+        Sum += list1[i]; // Добавляем текущий элемент к сумме
+        i++;
+    }
+    cout << "Сумма значений элементов массива с циклом while: " << Sum << endl;
+    */
+
+    //Задание 2 
+    sred = Sum / size(list1);
+    cout << "Среднее значение = " << sred << endl;
+
+    //Задание 3
+    for (int i = 0; i < size(list1); i++)
+    {
+        if (list1[i] >= 0)
+        {
+            cout << "Положительные элементы:  " << list1[i] << endl;
+        }
+        else
+        {
+            cout << "Отрицательные элементы:  " << list1[i] << endl;
+        }
+    }
+
+    //Задание 4
+    for (int i = 0; i < size(list1); i++)
+    {
+        if (list1[i] % 2 == 0)
+        {
+            cout << "Чётные элементы: " << list1[i] << endl;
+
+        }
+        else
+        {
+            cout << "Нечётные элементы: " << list1[i] << endl;
+        }
+    }
+}
+
